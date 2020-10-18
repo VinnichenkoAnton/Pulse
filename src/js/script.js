be8bbbf4f2502538580355f8df073a5a97836576
Anhurmes <example@gmail.com>
2020-10-18 19:09:07 +0300

    everything+tab

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icn/chevron_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icn/chevron_right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+
+    });
+});
