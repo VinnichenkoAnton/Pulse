f1e03edff0012e1d77c4ff1f9c24429306d7f802
Anhurmes <example@gmail.com>
2020-10-21 12:51:12 +0300

    validated forms

--- src/js/script.js
+++ src/js/script.js
@@ -31,4 +31,45 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consult]').on('click', function() {
+        $('.overlay, #consult').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consult, #thanks, #order').fadeOut('slow');
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свое почту",
+                    email: "Ваш email должен быть в формате name@domain.com"
+                }
+            }
+        });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consult form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7(999) 999-99-99");
 });
